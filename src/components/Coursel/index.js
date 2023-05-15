e12aed9d826e01278f6a3be83a9d7db0a25e6219
AmanPachori <amangirrajpachori2002@gmail.com>
2023-05-15 16:24:34 +0530

    coursel added

---
+++ src/components/Coursel/index.js
@@ -0,0 +1,48 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import Image from "next/legacy/image";
+
+import Image1 from "../../Garden360Images/SlidingBannerImage1.png";
+import Image2 from "../../Garden360Images/SlidingBannerImage2.png";
+const images = [Image1, Image2];
+const index = () => {
+  const [currentImage, setCurrentImage] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImage((prevImage) => (prevImage + 1) % images.length);
+    }, 3000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+  return (
+    <div
+      className="relative coursel border overflow-hidden"
+      style={{ height: "65vh" }}
+    >
+      <div className="flex transition-transform duration-300 ease-in-out transform-translate-x-full">
+        {images.map((image, index) => (
+          <div
+            key={index}
+            className={` h-full ${
+              index === currentImage ? "translate-x-0" : ""
+            } animate-slide`}
+          >
+            <Image
+              src={image.src}
+              layout="fill"
+              objectFit="cover"
+              objectPosition="center"
+              className="absolute inset-0 w-full h-full"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default index;
