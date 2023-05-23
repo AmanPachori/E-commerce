60917c821ba017447ab077ba9ce8684b5820adf5
AmanPachori <amangirrajpachori2002@gmail.com>
2023-05-23 14:17:33 +0530

    minor fixes

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -30,45 +30,12 @@ const Index = () => {
                 />
               </div>
               <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-dark mr-5 ">
-                Garden 360
+                Garden360
               </span>
             </a>
-            <div className="ml-8 relative self-center ">
-              <button
-                className=" text-black font-black flex justify-between w-full px-4 py-2 text-center text-dark-700 bg-white focus:outline-none focus:shadow-outline-blue"
-                onClick={toggleMenu}
-              >
-                More
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-4 h-4 self-center ml-2"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-                  />
-                </svg>
-              </button>
-              {isOpen && (
-                <div className="absolute z-10 w-56 mt-2 space-y-2 bg-white rounded-sm shadow-lg overflow-y-auto max-h-60 my-3">
-                  <ul className="py-1">
-                    <li className="px-4 py-2 hover:bg-gray-50">
-                      <a href="#">Option 1</a>
-                    </li>
-                    <li className="px-4 py-2 hover:bg-gray-50">
-                      <a href="#">Option 2</a>
-                    </li>
-                    <li className="px-4 py-2 hover:bg-gray-50">
-                      <a href="#">Option 3</a>
-                    </li>
-                  </ul>
-                </div>
-              )}
+            <div className="ml-8 relative self-center flex ">
+              <p className="m-2">Gardener</p>
+              <p className="m-2">Garden Setup</p>
             </div>
           </div>
           <div className="mx-10" id="navbar-dropdown">
@@ -76,7 +43,7 @@ const Index = () => {
               <li className="flex self-center">
                 <input
                   type="Search"
-                  className="w-full h-7 px-4 py-3 rounded-md border border-gray-300 focus:outline-none focus:border-gray-500"
+                  className="w-3/4 h-7 px-4 py-3 rounded-md border border-gray-300 focus:outline-none focus:border-gray-500"
                   placeholder="Search..."
                 />
                 <button>
