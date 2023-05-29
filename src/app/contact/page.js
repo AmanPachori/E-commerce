e4f95679568897828ce4095515e9075fd2ae46f6
AmanPachori <amangirrajpachori2002@gmail.com>
2023-05-29 12:02:29 +0530

    Implemented Responsive design for the page

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -41,34 +41,39 @@ const page = () => {
             </div>
           </form>
         </div>
-        <div className="flex items-center  justify-center ">
-          <div className="shadow-lg rounded-lg p-5 w-90 my-5  mx-auto">
-            <h2 className="text-center text-2xl mb-5 font-bold ">Contact Us</h2>
-            <div className="flex my-3 justify-center items-center text-center w-[40rem]">
-              <div className=" m-2 p-3 w-1/2 bg-gray-100 rounded ">
-                <div className="w-full flex items-center justify-center">
+        <div className="flex items-center justify-center">
+          <div className="shadow-lg rounded-lg p-5 my-5 mx-auto w-fit">
+            <h2 className="text-center text-2xl mb-5 font-bold">Contact Us</h2>
+            <div className="flex flex-col my-3 justify-center items-center text-center sm:flex-row sm:justify-between">
+              <div
+                style={{ width: "250px" }}
+                className="m-2 p-3 bg-gray-100 rounded w-250"
+              >
+                <div className="flex items-center justify-center">
                   <img
                     src="https://img.icons8.com/ios/30/new-post--v1.png"
                     alt="new-post--v1"
-                    className=""
                   />
                 </div>
-                <span> greenloverscare@gmail.com</span>
+                <span className="mt-2">greenloverscare@gmail.com</span>
               </div>
-              <div className="m-2 p-3 w-1/2 bg-gray-100 rounded ">
+              <div
+                style={{ width: "250px" }}
+                className="m-2 p-3 bg-gray-100 rounded w-250"
+              >
                 <a
                   href="https://api.whatsapp.com/send?phone=919306308583&text=&source=&data="
                   target="_blank"
+                  rel="noopener noreferrer"
                 >
-                  <div className="w-full flex items-center justify-center">
+                  <div className="flex items-center justify-center">
                     <img
                       src="https://img.icons8.com/ios/30/whatsapp--v1.png"
-                      alt="new-post--v1"
-                      className=""
+                      alt="whatsapp--v1"
                     />
                   </div>
                 </a>
-                <span>9306308583</span>
+                <span className="mt-2">9306308583</span>
               </div>
             </div>
           </div>
