8e897907924e43d769448b9cc272bb884f60d9cf
AmanPachori <amangirrajpachori2002@gmail.com>
2023-05-30 11:40:57 +0530

    minor updates

--- src/components/Gardener/index.js
+++ src/components/Gardener/index.js
@@ -3,13 +3,13 @@ import Image from "next/legacy/image";
 import Gardner from "../../Garden360Images/GardenerImage.jpg";
 const Index = () => {
   return (
-    <div className="flex p-5 my-3 h-75vh flex-col justify-center items-center text-center sm:flex-row sm:justify-between">
+    <div className="grid grid-cols-2 m-2">
       {/* Left Section */}
       <div className="flex-1 flex flex-col justify-center items-center  p-8">
         <a
           href="https://docs.google.com/forms/d/1IldDYr7J7y-ib8rD39KZeWKG39Qa_-Y1slSGe6R-qk4/viewform?edit_requested=true"
           target="_blank"
-          className="text-center text-[34px]  mb-5"
+          className="text-center text-[16px] md:text-[34px]  mb-5"
         >
           Book a Gardener.
         </a>
@@ -23,7 +23,7 @@ const Index = () => {
       </div>
 
       {/* Right Section */}
-      <div className="flex-1 border">
+      <div className="flex-2">
         <Image
           src={Gardner}
           alt="Image"
