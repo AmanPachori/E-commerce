6a9c16fa97317d0d9b84a274cf6e1797bb75b122
AmanPachori <amangirrajpachori2002@gmail.com>
2023-06-05 19:03:59 +0530

    minor fixes

--- src/components/Gardener/index.js
+++ src/components/Gardener/index.js
@@ -16,7 +16,7 @@ const Index = () => {
         <a
           href="https://docs.google.com/forms/d/1IldDYr7J7y-ib8rD39KZeWKG39Qa_-Y1slSGe6R-qk4/viewform?edit_requested=true"
           target="_blank"
-          className="border border-green-600 px-4 py-1  md:py-3 text-green-500 font-semibold mt-3 h-8 md:h-14 text-sm md:text-2xl"
+          className="border border-green-600 px-1 sm:px-4 py-2  md:py-3 text-green-500 font-semibold mt-3 h-8 md:h-14 text-xs sm:text-sm md:text-2xl"
         >
           Book Now !!
         </a>
