bc653d71b3ce7bb23a777903e043fc6895cbd770
AmanPachori <amangirrajpachori2002@gmail.com>
2023-06-08 12:06:58 +0530

    minor fixes

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,19 @@
 import { Navbar, Footer } from "@/components";
 import Homepage from "./Home";
+import Head from "next/head";
 export default function Main() {
   return (
     <div>
+      <Head>
+        <title>
+          Garden360 - Gardening Services: Book A Mali, Design & Setup Your
+          Garden
+        </title>
+        <meta
+          name="description"
+          content="Garden360 provides all gardening solutions, from renting a Mali to garden designing, setup & maintenance. We cover all types of gardens, such as indoor gardens/ outdoor gardens/ office gardens/ terrace gardens/ balcony gardens/ vertical gardens/ kitchen gardening/ landscaping, etc."
+        />
+      </Head>
       <Navbar />
       <Homepage />
       <Footer />
