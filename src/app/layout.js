fe1fbc0d0a27d1dd5042426ec5d17ac2a38fbf8f
AmanPachori <amangirrajpachori2002@gmail.com>
2023-06-08 11:56:50 +0530

    minor fixes

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,14 +6,22 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title:
     "Garden360 - Gardening Services: Book A Mali, Design & Setup Your Garden",
-  description:
-    "Garden360 provides all gardening solutions, from renting a Mali to garden designing, setup & maintenance. We cover all types of gardens, such as indoor gardens/ outdoor gardens/ office gardens/ terrace gardens/ balcony gardens/ vertical gardens/ kitchen gardening/ landscaping, etc.",
+  description: "",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <title>
+          Garden360 - Gardening Services: Book A Mali, Design & Setup Your
+          Garden
+        </title>
+        <meta
+          name="description"
+          content="Garden360 provides all gardening solutions, from renting a Mali to garden designing, setup & maintenance. We cover all types of gardens, such as indoor gardens/ outdoor gardens/ office gardens/ terrace gardens/ balcony gardens/ vertical gardens/ kitchen gardening/ landscaping, etc."
+          key="desc"
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
