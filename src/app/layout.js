f57ebceae9595c090fdd90a6d9e3708598594295
AmanPachori <amangirrajpachori2002@gmail.com>
2023-06-08 20:12:45 +0530

    minor fixes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
+      <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
@@ -31,7 +32,7 @@ export default function RootLayout({ children }) {
           name="og:title"
           content="Garden360 - Gardening Services: Book A Mali, Design & Setup Your Garden"
         />
-      </head>
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
