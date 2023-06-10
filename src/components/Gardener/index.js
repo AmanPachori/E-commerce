d35d3115c41f98396dca86484d88271ed7cb6551
AmanPachori <amangirrajpachori2002@gmail.com>
2023-06-10 23:00:30 +0530

    minor fixes

--- src/components/Gardener/index.js
+++ src/components/Gardener/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/legacy/image";
-import Gardner from "../../Garden360Images/GardenerImage.jpg";
+import Gardner from "../../Garden360Images/GardenerImage.png";
 const Index = () => {
   return (
     <div className="grid grid-cols-2 mx-2 my-1">
@@ -16,9 +16,9 @@ const Index = () => {
         <a
           href="https://docs.google.com/forms/d/1IldDYr7J7y-ib8rD39KZeWKG39Qa_-Y1slSGe6R-qk4/viewform?edit_requested=true"
           target="_blank"
-          className="border border-green-600 px-1 sm:px-4 py-2  md:py-3 text-green-500 font-semibold mt-3 h-8 md:h-14 text-xs sm:text-sm md:text-2xl"
+          className="border border-green-600 px-1 sm:px-4 py-2  md:py-3 text-green-500 font-semibold mt-3 h-8 md:h-14 text-sm sm:text-sm md:text-2xl"
         >
-          Book Now !!
+          Book Now
         </a>
       </div>
 
