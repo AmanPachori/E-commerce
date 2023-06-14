e7db56d13f96d4a43ebaf1ca26b8a504c47a116d
AmanPachori <amangirrajpachori2002@gmail.com>
2023-06-14 22:50:54 +0530

    minor fixes

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,7 +6,8 @@ import { NextSeo } from "next-seo";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "",
+  title:
+    "Garden360 - Gardening Services: Book A Mali, Design & Setup Your Garden",
   description:
     "Garden360 provides all gardening solutions, from renting a Mali to garden designing, setup & maintenance. We cover all types of gardens, such as indoor gardens/ outdoor gardens/ office gardens/ terrace gardens/ balcony gardens/ vertical gardens/ kitchen gardening/ landscaping, etc.",
 };
