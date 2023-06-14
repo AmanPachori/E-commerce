c9c0f70491eb91eaaec9890ed8b7c30782c4d7b4
AmanPachori <amangirrajpachori2002@gmail.com>
2023-06-14 22:41:58 +0530

    minor fixes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Head from "next/head";
+import { NextSeo } from "next-seo";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,23 +15,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,500;0,600;0,700;1,500;1,600;1,700&display=swap"
-          rel="stylesheet"
-        />
-        <title>
-          Garden360 - Gardening Services: Book A Mali, Design & Setup Your
-          Garden
-        </title>
-        <meta
-          name="og:description"
-          content="Garden360 provides all gardening solutions, from renting a Mali to garden designing, setup & maintenance. We cover all types of gardens, such as indoor gardens/ outdoor gardens/ office gardens/ terrace gardens/ balcony gardens/ vertical gardens/ kitchen gardening/ landscaping, etc."
-        />
-        <meta
-          name="og:title"
-          content="Garden360 - Gardening Services: Book A Mali, Design & Setup Your Garden"
+        <NextSeo
+          title="Simple Usage Example"
+          description="A short description goes here."
         />
       </Head>
       <body className={inter.className}>{children}</body>
--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,9 @@
 import { Navbar, Footer } from "@/components";
 import Homepage from "./Home";
-import Head from "next/head";
+
 export default function Main() {
   return (
     <div>
-      <Head>
-        <title>
-          Garden360 - Gardening Services: Book A Mali, Design & Setup Your
-          Garden
-        </title>
-        <meta
-          name="description"
-          content="Garden360 provides all gardening solutions, from renting a Mali to garden designing, setup & maintenance. We cover all types of gardens, such as indoor gardens/ outdoor gardens/ office gardens/ terrace gardens/ balcony gardens/ vertical gardens/ kitchen gardening/ landscaping, etc."
-        />
-        <meta
-          name="og:title"
-          content="Garden360 - Gardening Services: Book A Mali, Design & Setup Your Garden"
-        />
-      </Head>
       <Navbar />
       <Homepage />
       <Footer />
